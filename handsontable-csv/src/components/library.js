b61caf8a678ff7f9861c25f5322ac66b90b339f4
substrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2022-08-22 00:37:19 +0900

    csv 파일 파싱하기
    https://bloodstrawberry.tistory.com/288

--- handsontable-csv/src/components/library.js
+++ handsontable-csv/src/components/library.js
@@ -1,42 +1,90 @@
 // library.js
+const DELIMITER = ",";
+const APOSTROPHE = '"';
+
 export const handleOnDragLeave = (e, setState) => {
-    e.preventDefault();
-    setState(false);
-    return false;
+  e.preventDefault();
+  setState(false);
+  return false;
+};
+
+export const handleDragOver = (e, setState) => {
+  e.preventDefault();
+  setState(true);
+  return false;
+};
+
+export const handleOnDrop = (e, setState, csvObject) => {
+  e.preventDefault();
+
+  let file = e.dataTransfer.files[0];
+  let fileReader = new FileReader();
+
+  fileReader.readAsText(file, "utf-8"); // or euc-kr
+
+  fileReader.onload = function () {
+    //console.log(fileReader.result);
+    parsingCsv(fileReader.result, csvObject);
+    return;
   };
-  
-  export const handleDragOver = (e, setState) => {
-    e.preventDefault();
-    setState(true);
-    return false;
+
+  setState(false);
+  return false;
+};
+
+export const handleUpload = (e, csvObject) => {
+  let file = e.target.files[0];
+  let fileReader = new FileReader();
+
+  if (file === undefined) return; /* 방어 코드 추가 */
+
+  fileReader.readAsText(file, "utf-8"); // or euc-kr
+
+  fileReader.onload = function () {
+    //console.log(fileReader.result);
+    parsingCsv(fileReader.result, csvObject);
   };
-  
-  export const handleOnDrop = (e, setState) => {
-    e.preventDefault();
-  
-    let file = e.dataTransfer.files[0];
-    let fileReader = new FileReader();
-  
-    fileReader.readAsText(file, "utf-8"); // or euc-kr
-  
-    fileReader.onload = function () {
-      console.log(fileReader.result); 
-      return;
-    };
-  
-    setState(false);
-    return false; 
-  };
-  
-  export const handleUpload = (e) => {
-    let file = e.target.files[0];
-    let fileReader = new FileReader();
-    
-    if(file === undefined) return; /* 방어 코드 추가 */
-      
-    fileReader.readAsText(file, "utf-8"); // or euc-kr
-  
-    fileReader.onload = function () {
-      console.log(fileReader.result); 
-    };
+};
+
+export const mySplit = (line, delimiter, ignore) => {
+  let spt = [];
+  let tmp = "";
+  let flag = false;
+
+  for (let i = 0; i < line.length; i++) {
+    if (ignore === line[i] && flag === true) {
+      flag = false;
+      continue;
+    } else if (ignore === line[i]) {
+      flag = true;
+      continue;
+    }
+
+    if (line[i] === delimiter && flag === false) {
+      spt.push(tmp);
+      tmp = "";
+
+      continue;
+    }
+
+    tmp += line[i];
   }
+
+  spt.push(tmp);
+
+  return spt;
+};
+
+export const parsingCsv = (file) => {
+  let sptLine = file.split(/\r\n|\n/);
+  console.log(sptLine);
+
+  for (let line of sptLine) {
+    if (line === "") continue;
+
+    let spt = mySplit(line, DELIMITER, APOSTROPHE);
+    console.log(spt);
+  }
+
+  return;
+};
