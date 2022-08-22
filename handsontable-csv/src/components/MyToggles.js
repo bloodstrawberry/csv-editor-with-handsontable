fe7fe868b57c09200d7af58f905c0304e070836c
substrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2022-08-23 00:38:10 +0900

    Local Storage로 현재 version 기억하기
    https://bloodstrawberry.tistory.com/382

--- handsontable-csv/src/components/MyToggles.js
+++ handsontable-csv/src/components/MyToggles.js
@@ -3,29 +3,39 @@ import React, { useState, useEffect } from "react";
 import * as mnode from "./nodelibrary";
 import "../css/MyToggles.scss";
 
-const MyToggles = ({version, setVersion}) => {
+const MyToggles = ({ version, setVersion }) => {
+  const LOCAL_STORAGE = {
+    LS_VERSION: setVersion,
+  };
+
   const [versionList, setVersionList] = useState([]);
 
-  const onClickToggles = (value, setState) => {
+  const onClickToggles = (value, setState, lsKey) => {
     setState(value);
+    if(lsKey) localStorage.setItem(lsKey, value);
   };
 
   const makeVersion = () => {
-    if(versionList.length === 0) return;
-    
+    if (versionList.length === 0) return;
+
     return versionList.map((ver, idx) => (
       <div
         key={idx}
-        className={ver === version? "btn-child selected" : "btn-child"}
-        onClick={(e) => onClickToggles(e.target.innerText, setVersion)}
+        className={ver === version ? "btn-child selected" : "btn-child"}
+        onClick={(e) => onClickToggles(e.target.innerText, setVersion, 'LS_VERSION')}
       >
         {ver}
       </div>
     ));
-  }
+  };
 
   const init = () => {
     mnode.getVersionList(setVersionList);
+
+    for (let key in LOCAL_STORAGE) {
+      let data = localStorage.getItem(key);
+      if (data !== null) onClickToggles(data, LOCAL_STORAGE[key], key);
+    }
   };
 
   useEffect(init, []);
